07b12672cba049c0436930af783ac90bead23aeb
Tatiana Smetanina <smetaninatv@yandex.ru>
2017-02-10 19:32:25 +0000

    login page

--- GS.Recruitment.Web/Controllers/HomeController.cs
+++ GS.Recruitment.Web/Controllers/HomeController.cs
@@ -13,23 +13,10 @@ namespace GS.Recruitment.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Terms()
         { 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
